492cfcdb33ef7dcb550cb0198406a3cb52014f7d
monkeydj <monkeydjiotaro@gmail.com>
2017-08-03 09:38:28 +0700

    initialize generating script

--- generate.js
+++ generate.js
@@ -1,7 +1,8 @@
 //  A script to generate new microservice
 // @created 2017-07-27 16:38:10
 
-var fs = require("file-system"), path = require("path");
+var fs = require("fs-extra"), path = require("path");
+var minimatch = require("minimatch");
 // var cp = require("child_process");
 var parseGitignore = require("parse-gitignore");
 
@@ -13,20 +14,39 @@ var args = require("yargs")
 
 const { Skeleton, EcoSpace, EcoRoot } = require("./constants.js");
 
-if (EcoSpace == undefined) throw new Error("A space is a void anyway");
+if (!EcoSpace) throw new Error("A space is a void anyway...");
 
 var microservices = args._.concat(args.services);
 var ignores = parseGitignore(path.join(Skeleton, ".gitignore"), [".vscode"]);
 
-for (let mus of microservices) {
-    
-    console.info(`Start generating ${mus}...`);
-    
-    mus = path.join(EcoSpace, mus); // full path to microservice source
-    fs.copySync(Skeleton, mus, { noProcess: ignores, process });
+var intoBlkHoles = ignores.map(ig => {
+    // var trash = path.join(Skeleton, ig);
+    return minimatch.filter(ig, { matchBase: true });
+});
 
-    function process(content, filepath, relative) {
-        console.log(`Adding file ${filepath}... >>> ${relative}`);
-    }
+console.log(ignores, minimatch(path.join(Skeleton, ignores[0]), path.join(Skeleton, "dist/source")));
+
+// for (let mus of microservices) {
+//     try { generate(mus); }
+//     catch (e) { console.infoi(`ERROR: ${e.message}`), console.error(e); }
+// }
+
+function generate(service) {
+
+    console.info(`Start generating ${service}...`);
+
+    var opts = { errorOnExist: true, filter: handleStream };
+    // full path to microservice source
+    var srcBranch = path.join(EcoRoot, EcoSpace, service);
+
+    fs.copySync(Skeleton, srcBranch, opts);
+
+}
+
+function handleStream(src, dest) {
+
+    console.log(src, '>>', dest);
+
+    return true;
 
 }
